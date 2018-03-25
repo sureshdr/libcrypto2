8074da530b87dedfa8998d3ccd268977389b52e6
Dr. Suresh Ramasamy <sureshdr@gmail.com>
2018-03-25 22:18:19 +0800

    Add files via upload
    Master Commit

---
+++ Hashes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace sureshdr.libcrypto2
+{
+    public class Hashes
+    {
+        public static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+        public static string CalculateSHA256(string filename)
+        {
+
+            using (var stream = File.OpenRead(filename))
+            {
+                var hash = new SHA256Managed();
+                byte[] checksum = hash.ComputeHash(stream);
+                return BitConverter.ToString(checksum).Replace("-", String.Empty);
+            }
+        }
+    }
+}
